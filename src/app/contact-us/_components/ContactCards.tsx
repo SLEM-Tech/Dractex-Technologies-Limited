956d56b0d3c2ed7e244f316314273f7454ec7102
unknown <charleskeyz2017@gmail.com>
2026-03-09 16:25:47 +0100

    commit code

--- src/app/contact-us/_components/ContactCards.tsx
+++ src/app/contact-us/_components/ContactCards.tsx
@@ -8,61 +8,52 @@ import { IoLocationOutline } from "react-icons/io5";
 import { RxEnvelopeClosed } from "react-icons/rx";
 
 const ContactCards = () => {
-	const { data: generalSettings, isLoading, isError } = useGeneralSettings();
+  const { data: generalSettings, isLoading, isError } = useGeneralSettings();
 
-	const GeneralSettings: WooCommerceSetting[] = generalSettings;
+  const GeneralSettings: WooCommerceSetting[] = generalSettings;
 
-	// console.log("GeneralSettings", GeneralSettings);
+  // console.log("GeneralSettings", GeneralSettings);
 
-	const contactCardData = [
-		{
-			id: 1,
-			title: "Email Us",
-			type: "email",
-			icon: <RxEnvelopeClosed className='text-primary text-2xl xl:text-4xl' />,
-			additionalText: GeneralSettings ? GeneralSettings[0]?.value : "N/A",
-		},
-		{
-			id: 2,
-			title: "Call Us",
-			icon: <FiPhoneCall className='text-primary text-2xl xl:text-4xl' />,
-			type: "tel",
-			additionalText: GeneralSettings ? GeneralSettings[1]?.value : "N/A",
-		},
-		{
-			id: 3,
-			title: "Location",
-			type: "text",
-			icon: <IoLocationOutline className='text-primary text-2xl xl:text-4xl' />,
-			description: GeneralSettings ? GeneralSettings[2]?.value : "N/A",
-		},
-		// Add more contact card data here if needed
-	];
-	return (
-		<>
-			{generalSettings &&
-				contactCardData?.map((card) => (
-					<ContactCard
-						key={card.id}
-						isLoading={isLoading}
-						type={card.type}
-						title={card.title}
-						icon={card.icon}
-						additionalText={card.additionalText}
-						// additionalText2={card.additionalText2}
-						description={card.description}
-					/>
-				))}
-
-			{isLoading && (
-				<>
-					<Skeleton className='flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse' />
-					<Skeleton className='flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse' />
-					{/* <Skeleton className='flex flex-col w-[15rem] bg-slate-200 slg:w-[18rem] xl:w-[23rem] shadow-md rounded-md py-5 sm:py-20 xl:py-32 items-center justify-center text-center px-4 xl:px-1 animate-pulse' /> */}
-				</>
-			)}
-		</>
-	);
+  const contactCardData = [
+    {
+      id: 1,
+      title: "Email Us",
+      type: "email",
+      icon: <RxEnvelopeClosed className="text-primary text-2xl xl:text-4xl" />,
+      additionalText: "britney.silve-r-@email4.in",
+    },
+    {
+      id: 2,
+      title: "Call Us",
+      icon: <FiPhoneCall className="text-primary text-2xl xl:text-4xl" />,
+      type: "tel",
+      additionalText: "8084564240",
+    },
+    {
+      id: 3,
+      title: "Location",
+      type: "text",
+      icon: <IoLocationOutline className="text-primary text-2xl xl:text-4xl" />,
+      description: "AYINLA MUFUTAU, 12, Harmony est/ate Osagie okeira nla Ajah",
+    },
+    // Add more contact card data here if needed
+  ];
+  return (
+    <>
+      {contactCardData?.map((card) => (
+        <ContactCard
+          key={card.id}
+          isLoading={false}
+          type={card.type}
+          title={card.title}
+          icon={card.icon}
+          additionalText={card.additionalText}
+          // additionalText2={card.additionalText2}
+          description={card.description}
+        />
+      ))}
+    </>
+  );
 };
 
 export default ContactCards;
